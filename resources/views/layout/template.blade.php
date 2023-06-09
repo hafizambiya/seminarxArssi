41d0ef1c4565e8e7c2f143887fe35b1a56da50e1
Hafiz Ambiya <hafizambiya5@gmail.com>
2023-06-09 10:59:11 +0700

    mengubah url di template

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -34,7 +34,7 @@
       /* background-color:red; */
     }
   }
-  
+
   @media(min-width:850px){
       .daftar {
       margin-bottom: 20px;
@@ -61,7 +61,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
       <div class="container px-4">
-        <a class="navbar-brand"  href="">
+        <a class="navbar-brand"  href="{{ url('/peserta') }}">
           <img style="height:70px" src="{{ asset('/') }}assets/img/brand/arssi.png" />
           <h2 class="logo-arssi">Asosiasi Rumah Sakit Swasta Indonesia</h2>
         </a>
