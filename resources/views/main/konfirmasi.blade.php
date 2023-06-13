156be944059130cba21f8926b4a8462a3fbcdb48
Hafiz Ambiya <hafizambiya5@gmail.com>
2023-06-13 11:31:14 +0700

    menambahkan button kembli

--- resources/views/main/konfirmasi.blade.php
+++ resources/views/main/konfirmasi.blade.php
@@ -9,6 +9,9 @@
         <div class="row justify-content-center">
           <div class="col-lg-5 col-md-6">
             <h1 class="text-white">Konfirmasi Pembayaran</h1>
+            <a href="{{ url('admin') }}">
+              <button type="button" class="btn btn-warning">kembali</button>
+            </a>
 
           </div>
         </div>
