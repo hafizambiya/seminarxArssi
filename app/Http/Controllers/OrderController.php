edd928a75a00571bde40ec27b9e4aff329520054
Hafiz Ambiya <hafizambiya5@gmail.com>
2023-06-14 14:51:22 +0700

    mengubah logic transaksi status

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -84,7 +84,7 @@ require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
         $order_id = substr($request->order_id, 0, -2);
         $hashed = hash("sha512", $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         if ($hashed == $request->signature_key) {
-            if ($request->transaction_status == 'capture') {
+            if ($request->transaction_status == 'capture' || $request->transaction_status == 'settlement') {
                 $order = Peserta::where('idpesanan', $order_id)->first();
 
                 if ($order) {
